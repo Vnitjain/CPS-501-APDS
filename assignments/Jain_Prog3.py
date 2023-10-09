d9b8550c6d817f119cbb89368096d02b0b9d4244
Vinit <jain.vinit14@gmail.com>
2023-10-08 21:06:08 -0400

    assignment 3

--- assignments/Jain_Prog3.py
+++ assignments/Jain_Prog3.py
@@ -1,19 +1,34 @@
+# Name : Vinit Kirit Jain
+# Creation Date : 2023-10-08
+""" Brief Description :
+    Writing a program to display a menu containing three geometrical shapes and waiting for the user to enter their dimensions to then display the 
+    perimeter/circumference and the area of the shape.
+"""
+
+# importing the shapes.py file to use their functions
 import shapes
 
+# asking the user for their name
 user_name = input("Please enter your username and press enter: ")
+# greeting the user by using their name
 print("Hello, " + user_name + "!. Hope you are doing well!!!")
 
 
+# function for showing the menu
 def show_menu():
+    # asking the user to select a shape and press enter and converting it to integer
     user_input = int(
         input(
-            "Select a shape to calculate area or perimeter:\n1. Circle\n2. Square\n3. Rectangle\n4. Exit\n"
+            "Select a shape to calculate area or perimeter and then press enter:\n1. Circle\n2. Square\n3. Rectangle\n4. Exit\n"
         )
     )
+    # returning the users input in integer
     return user_input
 
 
+# function to show the result
 def show_result(geometrical_shape, perimeter, area):
+    # if the shape is circle using a different string
     if geometrical_shape == "circle":
         print(
             "The circumference of the "
@@ -22,52 +37,81 @@ def show_result(geometrical_shape, perimeter, area):
             + str(perimeter)
         )
     else:
+        # using different string for the other shapes
         print(
             "THe perimeter of the " + str(geometrical_shape) + " is: " + str(perimeter)
         )
-    print("The area of the " + str(geometrical_shape) + " is: " + str(area))
+    # showing the area of the shape
+    print("The area of the " + str(geometrical_shape) + " is: " + str(area) + "\n")
 
 
-def main():
-    show_menu()
-    user_input = -1
-    while user_input != 4:
-        if user_input == 1:
+# defining a function to process the choice of the user
+def process_choice(choice):
+    # running a while loop until exit choice is received
+    while choice != 4:
+        # if the choice is circle
+        if choice == 1:
+            # printing the formulae for circumference and area of circle
             print("The formula for circumference of circle is 2*pi*radius.")
             print("The formula for area of circle is pi*(radius**2).")
+            # asking the user for the radius of the circle and converting it to float
             radius_of_circle = float(
                 input("Please enter the radius of the circle and press enter: ")
             )
+            # using the shapes file to get the circumference of circle
             circumference_of_circle = shapes.circumference_of_circle(radius_of_circle)
+            # using the shapes file to get the area of the cirlce
             area_of_circle = shapes.area_of_circle(radius_of_circle)
+            # showing the final result
             show_result("circle", circumference_of_circle, area_of_circle)
-        elif user_input == 2:
+        # if the choice is square
+        elif choice == 2:
+            # printing the formulae for perimeter and area of square
             print("The formula for perimeter of square is 4*length.")
-            print("The formula for area of rectangle is length**2.")
+            print("The formula for area of square is length**2.")
+            # asking the user for length of square end convert it to float
             length_of_square = float(
                 input("Please enter the length of the square and press enter: ")
             )
+            # calculating the perimeter and area by calling the functions from the shape.py file
             perimeter_of_square = shapes.perimeter_of_square(length_of_square)
             area_of_square = shapes.area_of_square(length_of_square)
+            # printing the final result
             show_result("square", perimeter_of_square, area_of_square)
-        elif user_input == 3:
+        # if the choice is rectangle
+        elif choice == 3:
+            # printing the formulae for the perimeter and area of rectangle
             print("The formula for perimeter of rectangle is 2*(length+width).")
             print("The formula for area of rectangle is length*width.")
+            # asking the user for length and width of the rectangle
             length_of_rectangle = float(
                 input("Please enter the length of the rectangle and press enter: ")
             )
             width_of_rectangle = float(
                 input("Please enter the width of the rectangle and press enter: ")
             )
+            # calculating the perimeter and area by calling the functions from the shape.py file
             perimeter_of_rectangle = shapes.perimeter_of_rectangle(
                 length_of_rectangle, width_of_rectangle
             )
             area_of_rectangle = shapes.area_of_rectangle(
                 length_of_rectangle, width_of_rectangle
             )
+            # printing the final result
             show_result("rectangle", perimeter_of_rectangle, area_of_rectangle)
-        user_input = show_menu()
+        # showing the menu
+        choice = show_menu()
+    # printing thank you at the end of the code
     print("Thank you!!")
 
 
+# defining a main function
+def main():
+    # taking the user input with show menu method
+    user_input = show_menu()
+    # process the user entered option
+    process_choice(user_input)
+
+
+# calling the main function
 main()
--- assignments/shapes.py
+++ assignments/shapes.py
@@ -1,28 +1,34 @@
+# function to calculate the area of circle
 def area_of_circle(radius):
     area = 3.14 * radius**2
     return area
 
 
+# function to calculate the circumference of circle
 def circumference_of_circle(radius):
     circumference = 3.14 * (radius * 2)
     return circumference
 
 
+# function to calculate the area of square
 def area_of_square(length_of_side):
     area = length_of_side**2
     return area
 
 
+# function to calculate the perimeter of square
 def perimeter_of_square(length_of_side):
     perimeter = 4 * length_of_side
     return perimeter
 
 
+# function to calculate the area of rectangle
 def area_of_rectangle(length_of_side, width_of_side):
     area = length_of_side * width_of_side
     return area
 
 
+# function to calculate the perimeter of rectangle
 def perimeter_of_rectangle(length_of_side, width_of_side):
     perimeter = (2 * length_of_side) + (2 * width_of_side)
     return perimeter
